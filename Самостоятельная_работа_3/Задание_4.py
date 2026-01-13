8211557a5c0f3a11d8dfc963edf2be73f1b59daa
bilanmm <matblmh72@gmail.com>
2026-01-14 03:21:16 +0500

    Самостоятельная_работа_3

---
+++ Самостоятельная_работа_3/Задание_4.py
@@ -0,0 +1,15 @@
+# TODO реализовать функцию
+
+def get_sentences_list(str_words):
+    sentences_list = []
+    for sentence in str_words.split("."):
+        if sentence:
+            sentences_list.append(sentence.strip())
+
+    return sentences_list
+
+
+print(get_sentences_list("Здесь много разных слов. Возможно и много повторений..."))
+
+
+
